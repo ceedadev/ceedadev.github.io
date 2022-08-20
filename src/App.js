f650c3c7a25fd57bd682ecc856c409da18697b6a
Armanda Arif <ceedadev@gmail.com>
2022-08-20 07:48:51 +0700

    installed flowbite

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="relative flex min-h-screen bg-slate-100">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
